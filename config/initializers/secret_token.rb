b2167927f005b81212fd7bbebadc162a1888abaa
lpelos <lucas.pelos@gmail.com>
2012-09-21 15:10:44 -0300

    criando aplicação

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheresmamoney::Application.config.secret_token = 'c1caad22b22f6b61d934494294e151c6674be325aef8eda6c46f9a0314a83123a4efdb683e59d4619c2079b9450173daa8f3619903a04301783e52cface91758'
