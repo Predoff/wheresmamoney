06f1d0a094864b4910a67270f48298720b3dffbd
lpelos <lucas.pelos@gmail.com>
2012-09-24 12:18:17 -0300

    Acrescentando user_id à tabela de tags

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
-  	
+  	@categories = Category.all.map { |category| category.name }
+  	@tags = Tag.all.map { |tag| tag.name }
   end
 end
