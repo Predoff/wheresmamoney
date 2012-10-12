05d77dc75034ca3ef7b336cf88e75ce49d1a335f
Pedro Souza <predoff@gmail.com>
2012-10-12 11:17:13 -0300

    Refatorando controller de Transações e corrigindo tradução utilizando locales.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,23 +1,17 @@
 # encoding: UTF-8
 class TransactionsController < ApplicationController
-	respond_to :js
+  respond_to :js
 
-	def index
-		@transactions = Transaction.where(user_id: 1).order('date DESC')
-		@incomes = @transactions.where(type: 'Income')
+  def index
+	@transactions = Transaction.where(user_id: 1).order('date DESC')
+	@incomes = @transactions.where(type: 'Income')
     @outgoes = @transactions.where(type: 'Outgo')
 
-    #@months = ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-    @months = { :'1' => 'Janeiro', :'2' => 'Fevereiro', :'3' => 'Março', :'4' => 'Abril', :'5' => 'Maio', :'6' => 'Junho', :'7' => 'Julho', :'8' => 'Agosto', :'9' => 'Setembro', :'10' => 'Outubro', :'11' => 'Novembro', :'12' => 'Dezembro'}
-  	#Março não funciona porque é UTF-8. Tem que arrumar isso.
-
-    @month_to_show = params[:'selected-month'] || Date.today.month
-    @year_to_show = params[:'selected-year'] || Date.today.year
-    
+    @date_to_show = Date.current
   end
 
   def destroy
   	Transaction.destroy(params[:id])
   end
-  
+
 end
