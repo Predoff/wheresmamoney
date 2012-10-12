1332eb939784359b1c00c38788d8f7fce0e40b50
lpelos <lucas.pelos@gmail.com>
2012-10-12 13:36:34 -0300

    Mergiando alterações por Pedroff

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,29 +1,26 @@
 # encoding: UTF-8
 class TransactionsController < ApplicationController
-	respond_to :js
+  respond_to :js
 
 	def index
     #@incomes = Income.includes(:source)
     #@outgoes = Outgo.includes(:tags, :category)
     #@transactions = @outgoes + @incomes
-
-		@transactions = Transaction.where(user_id: 1).order('date DESC')
-		@incomes = @transactions.where(type: 'Income').includes(:source)
-    @outgoes = @transactions.where(type: 'Outgo').includes(:tags, :category)
-
-    @months = { :'1' => 'Janeiro', :'2' => 'Fevereiro', :'3' => 'Março', :'4' => 'Abril', :'5' => 'Maio', :'6' => 'Junho', :'7' => 'Julho', :'8' => 'Agosto', :'9' => 'Setembro', :'10' => 'Outubro', :'11' => 'Novembro', :'12' => 'Dezembro'}
-
-    @month_to_show = params[:'selected-month'] || Date.today.month
-    @year_to_show = params[:'selected-year'] || Date.today.year
     
+  	@transactions = Transaction.where(user_id: 1).order('date DESC')
+  	@incomes = @transactions.where(type: 'Income')
+    @outgoes = @transactions.where(type: 'Outgo')
+
+    @date_to_show = Date.current
+    #@month_to_show = params[:'selected-month'] || Date.today.month
+    #@year_to_show = params[:'selected-year'] || Date.today.year
   end
 
   def destroy
   	Transaction.destroy(params[:id])
   end
-  
-end
 
+end
 
 # Examinar se se aplica ao meu sistema esse tipo de funcionalidade e implementar
 #
