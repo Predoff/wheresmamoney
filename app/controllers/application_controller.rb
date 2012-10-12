142589e7a11e7003d05c0470dd629cbfaa076dc2
Pedro Souza <predoff@gmail.com>
2012-10-11 22:44:16 -0300

    Refatorando controller de Gastos e Ganhos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def current_user
+    User.find 1
+  end
 end
--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,13 +1,8 @@
 class IncomesController < ApplicationController
+  def create
+    params[:income][:value] = params[:income][:value].gsub(',', '.')
 
-	def create
-    value = params[:income][:value].gsub(',', '.')
-    date = params[:income][:date]
-    description = params[:income][:description]
-    user_id = 1
-    source_id = params[:income][:source_id]
-
-    @income = Income.new( value: value, date: date, description: description, source_id: source_id, user_id: user_id )
-  	@income.save
+    @income = current_user.incomes.build(params[:income])
+    @income.save
   end
 end
