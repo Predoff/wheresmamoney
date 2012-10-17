31578b735788049f0e50942b48d2c46c2429422f
lpelos <lucas.pelos@gmail.com>
2012-10-16 22:26:02 -0300

    Removendo controller obsoleto: dashboard

--- app/controllers/dashboard_controller.rb
+++
@@ -1,9 +0,0 @@
-class DashboardController < ApplicationController
-	
-  #def index
-  #	@user_name = current_user.name
-  #	@categories = current_user.categories.order('name')
-  #	@sources = current_user.sources.order('name')
-  #	@tags = current_user.tags.order(:name).map { |tag| tag.name }
-  #end
-end
